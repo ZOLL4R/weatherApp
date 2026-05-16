7228528ce95b7b6a0fe7c368a1655c4de756b3a5
Zoll4r <mohamedassaadbouarich@gmail.com>
2026-05-16 14:31:09 +0100

    JavaScript adjusment

--- page 2/page2.js
+++ page 2/page2.js
@@ -5,83 +5,109 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const SearchedCity = document.querySelector(".search-bar input");
 const Search_btn = document.querySelector(".search-bar button");
 
+let cityTable = []; 
 
                     //FONCTION TO CHECK THE WEATHER------------------------------------
 
 
 async function checkWeather(NameCity) { 
 
-    const weatherResponse = await fetch(apiURL + NameCity + `&appid=${apiKey}`);
-    var data = await weatherResponse.json();
+    // Kanden mn ligne 16 --> 89 hia function checkWeather 
+    if (NameCity === "") return;
 
-    if (Response.status == 404){
-        document.querySelector(".errorsMSG").style.display = "block";
+    
+    const weatherResponse = await fetch(apiURL + NameCity + `&appid=${apiKey}`);
+
+    const data = await weatherResponse.json();
+
+    if (weatherResponse.status == 404){
+        document.querySelector(".errorsMSG_Name").style.display = "block";
         document.querySelector(".weatherChart").style.display = "none";
     }else{
         document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".T").innerHTML = data.main.temp + '°C';
-    document.querySelector(".H").innerHTML = data.main.humidity + '%';
-    document.querySelector(".WS").innerHTML = data.wind.speed + ' Km/h';
-    
-    console.log(data);
-    
+        document.querySelector(".T").innerHTML = data.main.temp + '°C';
+        document.querySelector(".H").innerHTML = data.main.humidity + '%';
+        document.querySelector(".WS").innerHTML = data.wind.speed + ' Km/h';
+        
+        
 
-                        //CHANGING THE IMAGES OF THE EQUIVALENT WEATHER ----------------------
-    
-    const weatherImg = document.querySelector(".WeatherImg");
-    var weatherInfo = data.weather[0].main;
-    switch (weatherInfo) {
+                            //CHANGING THE IMAGES OF THE EQUIVALENT WEATHER ----------------------
+        
+        const weatherImg = document.querySelector(".WeatherImg");
+        var weatherInfo = data.weather[0].main;
+        switch (weatherInfo) {
 
-    case "Clear":
-        weatherImg.src ="image/sunny.png";
-        break;
-    
-    case "Clouds":
-        weatherImg.src ="image/cloud.png";
-        break;
-    
-    case "Rain":
-        weatherImg.src ="image/rain.png";
-        break;
+        case "Clear":
+            weatherImg.src ="image/sunny.png";
+            break;
+        
+        case "Clouds":
+            weatherImg.src ="image/cloud.png";
+            break;
+        
+        case "Rain":
+            weatherImg.src ="image/rain.png";
+            break;
 
-    case "Snow":
-        weatherImg.src ="image/freeze.png";
-        break;
+        case "Snow":
+            weatherImg.src ="image/freeze.png";
+            break;
 
-    case "Drizzle":
-        weatherImg.src ="image/drizzle.png";
-        break;
+        case "Drizzle":
+            weatherImg.src ="image/drizzle.png";
+            break;
 
-    case "Thunderstorm":
-        weatherImg.src ="image/sunny.png";
-        break;
+        case "Thunderstorm":
+            weatherImg.src ="image/sunny.png";
+            break;
 
-    case "Mist":
-        weatherImg.src ="image/sunny.png";
-        break;
+        case "Mist":
+            weatherImg.src ="image/sunny.png";
+            break;
+
+        }   
+
+        document.querySelector(".weatherChart").style.display = "block";
+
+        const foundCity = cityTable.find(element => element === data.name);
+        if (!foundCity){
+            cityTable.push(data.name);
+            addToHistorique(NameCity);
+        }  
+
+        
+
+        }
 
 }
 
-    document.querySelector(".weatherChart").style.display = "block";
-    }
-
+ function addToHistorique(NameCity) {
     
+        const $history = document.querySelector(".les_historiques");
+        const $newHistory = document.createElement("li");
+        const $k = document.createDocumentFragment();
 
+        $newHistory.innerHTML = `${NameCity} <div class="controleBTN"><button class="up"><img src="image/up_arrow.png" alt="go up"></button>  <button class="down"><img src="image/down_arrow.png" alt="go down"></button>  <button class="delete"><img src="image/delete.png" alt="delete"></button></div>`;
+        $k.append($newHistory);
+        $history.append($k);
+    } 
 
+        
 
-}
 
                     // BUTTON --------------------------------------------
 
 
 Search_btn.addEventListener("click", ()=>{
     checkWeather(SearchedCity.value);
+
 })
 
 
 
 
 
+//--------------------Historique---------------------
 
 
 
